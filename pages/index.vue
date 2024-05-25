f420b5e89567aa29d6f325c0248fa8b87f180410
juliadyrdal <juliadyrdal@protonmail.com>
2024-05-25 03:46:21 +0200

    Hide form until mounted.

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { useRouter, useRoute } from 'vue-router';
 import { useSocketStore } from '@/stores/socket';
-import { onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 
 const router = useRouter();
 const route = useRoute();
@@ -10,13 +10,17 @@ const socketStore = useSocketStore();
 const quizId = ref('');
 const playerName = ref('');
 const quizIdIsSet = ref(false);
+const mounted = ref(false);
 
 onMounted(() => {
   const queryQuizId = route.query.quizId;
+  // Hide input field for quiz ID if it is included in the route
   if (queryQuizId) {
     quizId.value = queryQuizId;
     quizIdIsSet.value = true;
   }
+  // Indicate component has been mounted
+  mounted.value = true;  
 });
 
 const handleSubmit = () => {
@@ -32,18 +36,12 @@ const handleSubmit = () => {
 </section>
 <section class="container px-6">
   <h3 class="pt-16 pb-8 text-xl font-semibold text-center">Join quiz</h3>
-  <form @submit.prevent="handleSubmit">
-  <label for="playerName" class="block mb-2">What do you want to call yourself?</label>
-  <input type="text" v-model="playerName" class="mb-6 pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" id="playerName">
-  <label :class="{ hidden: quizIdIsSet }" for="quizId" class="block mb-2">Enter the quiz ID</label>
-  <input :class="{ hidden: quizIdIsSet }" type="text" v-model="quizId" class="mb-4 pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" id="quizId">
-  <button type="submit" class="mt-3 w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Join quiz</button>
-</form>
+  <form v-if="mounted" @submit.prevent="handleSubmit">
+    <label for="playerName" class="block mb-2">What do you want to call yourself?</label>
+    <input type="text" v-model="playerName" class="mb-6 pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" id="playerName">
+    <label v-if="!quizIdIsSet" for="quizId" class="block mb-2">Enter the quiz ID</label>
+    <input v-if="!quizIdIsSet" type="text" v-model="quizId" class="mb-4 pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" id="quizId">
+    <button type="submit" class="mt-3 w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Join quiz</button>
+  </form>
 </section>
 </template>
-
-<style>
-.hidden {
-  display: none;
-}
-</style>
